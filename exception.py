ead54e818a96a92b3b3412a7a4d89760b21d3b22
Dilia-Galliamova <109653916+Dilia-Galliamova@users.noreply.github.com>
2022-08-11 17:06:49 +0300

    Delete exception.py

--- exception.py
+++
@@ -1,5 +0,0 @@
-def trigger_exception(index):
-    if str(index).isdigit() is False:
-        raise ValueError()
-    elif not (int(index) // 10 in [1, 2, 3, 4, 5, 6] and int(index) % 10 in [1, 2, 3, 4, 5, 6]):
-        raise ValueError()
